a30538b63f2d3bcfa3dc381f2f7311f7e23b5f15
Shane <shanemcneil19@gmail.com>
2019-11-04 19:46:58 -0700

    refactor articles-router and articles-endpoints.spec files

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -19,16 +19,12 @@ articlesRouter
         const { title, content, style } = req.body
         const newArticle = { title, content, style }
 
-        if (!title) {
-            return res.status(400).json({
-                error: { message: `Missing 'title' in request body`}
-            })
-        }
-
-        if (!content) {
-            return res.status(400).json({
-                error: { message: `Missing 'content' in request body`}
-            })
+        for (const [key, value] of Object.entries(newArticle)) {
+            if (value == null) {
+                return res.status(400).json({
+                    error: { message: `Missing '${key}' in request body` }
+                })
+            }
         }
 
         ArticlesService.insertArticle(
